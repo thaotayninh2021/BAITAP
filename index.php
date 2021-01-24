f59966b7ad2669312de2c204dcaff99197a178f3
thaotayninh2021 <77915619+thaotayninh2021@users.noreply.github.com>
2021-01-24 15:47:32 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+  <title>Spoon-Knife</title>
+  <LINK href="styles.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+
+<H3>Nguyen Trong Thao - VNPT Tay Ninh </H3>
+
+</body>
+</html>
